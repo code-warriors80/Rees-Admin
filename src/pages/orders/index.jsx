f11b90bb5fa70af9826831646cc800ba9c746050
youngsavage-codes <muctarmohammed07@gmail.com>
2023-10-29 15:31:44 +0100

    update

--- src/pages/orders/index.jsx
+++ src/pages/orders/index.jsx
@@ -1,121 +1,55 @@
 import { Link } from "react-router-dom";
 import { useOrder } from "../../context/ordersContext";
-import { useUser } from "../../context/userContext";
-import { useProduct } from "../../context/productContext";
-
-const ordersArray = [
-  {
-    id: 123456,
-    order: "Pizza Margherita",
-    user: "John Smith",
-  },
-  {
-    id: 789012,
-    order: "Chicken Alfredo Pasta",
-    user: "Alice Johnson",
-  },
-  {
-    id: 345678,
-    order: "Vegetable Stir-Fry",
-    user: "David Lee",
-  },
-  {
-    id: 901234,
-    order: "Burger with Fries",
-    user: "Emily Wilson",
-  },
-  {
-    id: 567890,
-    order: "Sushi Platter",
-    user: "Michael Davis",
-  },
-  {
-    id: 234567,
-    order: "Taco Salad",
-    user: "Sarah Brown",
-  },
-  {
-    id: 890123,
-    order: "Grilled Chicken Sandwich",
-    user: "Laura Martinez",
-  },
-  {
-    id: 456789,
-    order: "Spaghetti Bolognese",
-    user: "James Taylor",
-  },
-  {
-    id: 678901,
-    order: "Veggie Pizza",
-    user: "Olivia White",
-  },
-  {
-    id: 123789,
-    order: "Steak Dinner",
-    user: "William Turner",
-  },
-];
 
 const ListItem = ({ order }) => {
-  const { users } = useUser();
+  const orderDate = new Date(order.order.orderDate);
 
-  const orderUser = users
-    ? users.filter((user) => user._id === order.customer)
-    : "loading user...";
+  let formattedDate = "";
+  if (!isNaN(orderDate.getTime())) {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    formattedDate = orderDate.toLocaleDateString(undefined, options);
+  } else {
+    console.log("Invalid date");
+  }
+
+  const totalAmount = order.order.totalAmount.toLocaleString("en-NG", {
+    style: "currency",
+    currency: "NGN",
+  });
+
   return (
-    <Link
-      to={`/orders/${order.order._id}`}
-      className="flex items-center p-3 hover:bg-zinc-50 space-x-4"
-    >
+    <Link to={`/orders/${order.order._id}`} className="flex items-center p-3 hover-bg-zinc-50 space-x-4">
       <div className="flex-1 min-w-0">
-        <p className="text-sm font-medium text-gray-900 truncate ">
-          {orderUser.map((user, index) => (
-            <li
-              key={user.username}
-              className="text-sm list-none font-medium text-gray-900 truncate"
-            >
-              <span className="mr-2">{index + 1}.</span> {user.username}
-            </li>
-          ))}
-        </p>
-        <p className="text-sm text-gray-500 truncate">
-          {order.products.map((v) => (
-            <li key={v.name}>
-              <span className="mr-2">{v.name} </span>
-              <span>#{v.price}</span>
-            </li>
-          ))}
-        </p>
+        <p className="text-sm font-medium text-gray-900 truncate">{order.customer.username}</p>
+        <p className="text-sm font-medium text-gray-900 truncate">{order.order.products.length}</p>
+        <p className="text-sm text-gray-500 truncate"></p>
       </div>
-      <div className="inline-flex items-center text-base font-semibold text-gray-900 ">
-        {order.order.orderDate?.toLocaleString("en-NG", {
-          style: "currency",
-          currency: "NGN",
-        })}
-      </div>
+      <div className="inline-flex items-center text-base font-semibold text-gray-900">{totalAmount}</div>
+      <div className="text-sm text-gray-500">{formattedDate}</div>
     </Link>
   );
 };
 
+
 const Orders = () => {
-  const { orders, error, isLoading } = useOrder();
-  const content =
-    orders.length > 0
-      ? orders?.map((order) => <ListItem key={order.id} order={order} />)
-      : "No other at the moment";
+  const { orders, isLoading, error } = useOrder();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <section className="p-6">
       <h1 className="text-3xl font-bold">Orders</h1>
 
-      <select className="border mt-3 text-xs outline-none">
-        <option>select</option>
-        <option>Cooking</option>
-        <option>Delivered</option>
-      </select>
-
-      <section className="grid my-3 w-2/3 divide-y divide-gray-200">
-        {isLoading ? "loading..." : error ? "An error occured" : content}
+      <section className="grid my-8 w-2/3 divide-y divide-gray-200">
+        {orders.map((order, index) => (
+          <ListItem key={index} order={order} />
+        ))}
       </section>
     </section>
   );
--- src/pages/orders/order-id/index.jsx
+++ src/pages/orders/order-id/index.jsx
@@ -1,80 +1,19 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useUser } from "../../../context/userContext";
 
-const ordersArray = [
-  {
-    id: 123456,
-    order: "Pizza Margherita",
-    user: "John Smith",
-  },
-  {
-    id: 789012,
-    order: "Chicken Alfredo Pasta",
-    user: "Alice Johnson",
-  },
-  {
-    id: 345678,
-    order: "Vegetable Stir-Fry",
-    user: "David Lee",
-  },
-  {
-    id: 901234,
-    order: "Burger with Fries",
-    user: "Emily Wilson",
-  },
-  {
-    id: 567890,
-    order: "Sushi Platter",
-    user: "Michael Davis",
-  },
-  {
-    id: 234567,
-    order: "Taco Salad",
-    user: "Sarah Brown",
-  },
-  {
-    id: 890123,
-    order: "Grilled Chicken Sandwich",
-    user: "Laura Martinez",
-  },
-  {
-    id: 456789,
-    order: "Spaghetti Bolognese",
-    user: "James Taylor",
-  },
-  {
-    id: 678901,
-    order: "Veggie Pizza",
-    user: "Olivia White",
-  },
-  {
-    id: 123789,
-    order: "Steak Dinner",
-    user: "William Turner",
-  },
-];
-
-const TableItem = ({ order, qty }) => {
-  const { users } = useUser()
-
-    const orderUser = users ? users.filter(user => user._id === order.customer) : 'loading user...'
+const TableItem = ({ product, qty }) => {
   return (
-    <tr className="bg-white border-b ">
+    <tr className="bg-white border-b">
       <th
         scope="row"
-        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
       >
-        {orderUser.map((user, index) => (
-            <li key={user.username} className="text-sm list-none font-medium truncate">
-              <span className="mr-2">{index + 1}.</span> {user.username}
-            </li>
-          ))}
+        {product.name}
       </th>
-      <td className="px-6 py-4 text-center">#{order._id}</td>
-      <td className="px-6 py-4 text-center">{qty.reduce((total, item) => total + item.quantity, 0)}</td>
+      <td className="px-6 py-4 text-center">#{product._id}</td>
+      <td className="px-6 py-4 text-center">{qty}</td>
       <td className="px-6 py-4">
-        {order.totalAmount.toLocaleString("en-NG", {
+        {product.price.toLocaleString("en-NG", {
           style: "currency",
           currency: "NGN",
         })}
@@ -82,7 +21,7 @@ const TableItem = ({ order, qty }) => {
     </tr>
   );
 };
-const apiLink = process.env.REACT_APP_API_URL
+
 const ListItem = ({ text, value }) => {
   return (
     <span className="grid grid-cols-2 mb-1.5 items-center gap-x-20">
@@ -98,43 +37,38 @@ const ListItem = ({ text, value }) => {
 };
 
 const OrderId = () => {
-  let { id } = useParams();
-  const [order, setOrder] = useState([])
-  const { users } = useUser()
-  const orderUser = users ? users.filter(user => user._id === order.customer) : 'loading user...'
-  const [error, setError] = useState('')
-  const [isloading, setIsLoading] = useState(false)
+  const { id } = useParams();
+  const [orderDetails, setOrderDetails] = useState([]);
 
-
-  console.log(order,'order')
-  const getOrder = async () => {
-      setIsLoading(true)
+useEffect(() => {
+  const fetchOrderDetails = async () => {
     try {
-      const res = await fetch(`${apiLink}/order/order/${id}`, {
-        headers: {
-          "Content-type": "application/json",
-        }
-      })
-      const data = await res.json()
-      if (res.ok) {
-        setOrder([data])
-        setIsLoading(false)
-
+      const response = await fetch(`https://rees-kitchen.onrender.com/order/ordersingleProduct/${id}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch order details");
       }
-      if (!res.ok) {
-        setError("No product data at the moment")
-        setIsLoading(false)
-
-      }
+      const data = await response.json();
+      setOrderDetails(data);
     } catch (error) {
-      setError("No product data at the moment")
-      setIsLoading(false)
-
+      console.error(error);
     }
-  }
-  useEffect(() => {
-    getOrder()
-  }, [])
+  } 
+
+  fetchOrderDetails()
+}, [id])
+
+if (!orderDetails || orderDetails.length === 0) {
+  return <p>loading......</p>;
+}
+
+  
+  const order = orderDetails[0].order; // Access the order property
+  console.log(order);
+  const customer = orderDetails[0].customer; // Access the customer property
+  console.log(customer);
+  const products = orderDetails[0].products; // Access the products property
+  console.log(products);
+  const totalAmount = products.reduce((total, product) => total + product.product.price * product.quantity, 0);
   return (
     <section className="p-6">
       <h1 className="text-3xl font-bold">Orders / Order-details</h1>
@@ -143,24 +77,24 @@ const OrderId = () => {
         <div className="flex items-center space-x-4">
           <div className="flex-1 min-w-0">
             <p className="text-sm font-medium text-gray-900 truncate ">
-              {orderUser?.name}
+              {customer.username}
             </p>
             <p className="text-sm text-gray-500 truncate ">
-              {orderUser?.email}
+              {customer.email}
             </p>
           </div>
           <div className="inline-flex items-center text-base font-semibold text-gray-900 ">
-            Order ${order.id}
+            Order #{order._id}
           </div>
         </div>
         <div className="mt-6">
           <h3 className="text-zinc-400 font-bold">
-            Order Details ({order.length})
+            Order Details ({products.length})
           </h3>
 
           <div className="mt-8 relative overflow-x-auto">
             <table className="w-full text-sm text-left text-gray-500">
-              <thead className="text-xs text-white uppercase bg-[#F39300]">
+              <thead className="text-xs text-white uppercase bg-purple-600">
                 <tr>
                   <th scope="col" className="px-6 py-3">
                     Product name
@@ -177,22 +111,16 @@ const OrderId = () => {
                 </tr>
               </thead>
               <tbody>
-                {order.length > 0 ? order.map((order, index) => (
-                  <TableItem
-                    key={order._id}
-                    order={order}
-                    qty={order.products}
-                  />
-                )) : isloading ? 'Loading....' : error ? error : 'No other at the moment'}
+                {products.map((product) => (
+                  <TableItem key={product.product._id} product={product.product} qty={product.quantity} />
+                ))}
               </tbody>
             </table>
           </div>
 
-          <aside className="flex  justify-end mt-8">
+          <aside className="flex justify-end mt-8">
             <div>
-              {/* <ListItem text="Subtotal" value={1200} />
-              <ListItem text="Tax (3%)" value={36} /> */}
-              <ListItem text="Total" value={order.map(value => value.totalAmount)} />
+              <ListItem text="Subtotal" value={totalAmount} />
             </div>
           </aside>
         </div>
